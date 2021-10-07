20684e4c6a0fd47600e668efc0209d830971b3dc
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-07 11:00:13 -0400

    Added Router wrapper in index.js; Adjusted title in index.html

--- fleet_manager/src/App.js
+++ fleet_manager/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
 
   render() { 
-    const user = this.state.user;
+    //const user = this.state.user;
     return ( 
       <div className = "App">
         <Login />
