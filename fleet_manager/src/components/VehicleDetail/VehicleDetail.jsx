2927e8c38a8aaa29b5d649ca80ce61193533cb9d
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-08 12:59:47 -0400

    Modified formatting

--- fleet_manager/src/components/VehicleDetail/VehicleDetail.jsx
+++ fleet_manager/src/components/VehicleDetail/VehicleDetail.jsx
@@ -8,12 +8,12 @@ function VehicleDetails(props) {
 
 
     return (
-        <div>
-            <div>
-                <div>
+        <div class="card text-white bg-primary mb-3">
+            <div class = "card-header">
+                <div class = "card-body">
                     {props.location.state.vehicles.map((element) =>
                     <>
-                    <h3>Vehicle: {element.year} {element.make} {element.model}</h3>
+                    <h4>Vehicle: {element.year} {element.make} {element.model}</h4>
                     <h4>VIN: {element.VIN}</h4>
                     <h4>Mileage: {element.mileage}</h4>
                     <h3>Services Required: </h3>
@@ -23,6 +23,7 @@ function VehicleDetails(props) {
                 </div>
             </div>
         </div>
+
     )
 }
 export default VehicleDetails;
